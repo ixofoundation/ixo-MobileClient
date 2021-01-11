24e8b1f2ca3ec57957119590550be2ca9c92f3ec
gwn <dev@gwn.wtf>
2021-01-11 23:23:06 +0300

    Introduce storage

--- src/App.js
+++ src/App.js
@@ -2,9 +2,16 @@ const
     React = require('react'),
     {NavigationHandler} = require('navigation-react'),
     {NavigationStack} = require('navigation-react-native'),
+    AsyncStorage = require('@react-native-async-storage/async-storage').default,
     nav = require('./nav')
 
+
 nav.navigate('lorem')
+
+AsyncStorage.getItem('test')
+    .then(console.log)
+
+AsyncStorage.setItem('test', 'mest' + Date.now())
 
 
 module.exports = () =>
