cc4b2b001aa0ffd0fe54b1e6277868e3fc74f143
gwn <dev@gwn.wtf>
2021-01-13 20:50:21 +0300

    Implement mnemonic support

--- src/scenes/Lorem.js
+++ src/scenes/Lorem.js
@@ -1,19 +1,32 @@
 const
-    React = require('react'),
+    {useState, ...React} = require('react'),
     {View, Text, TouchableHighlight} = require('react-native'),
-    {withNav} = require('$/lib/nav')
+    {withNav} = require('$/lib/nav'),
+    {entropyToMnemonic} = require('bip39'),
+    {randomBytes} = require('react-native-randombytes')
 
 
-const Lorem = ({nav}) =>
-    <View>
-        <Text>lorem</Text>
+const Lorem = ({nav}) => {
+    const [mm, setMm] = useState('')
 
-        <TouchableHighlight
-            onPress={() => nav.stateNavigator.navigate('ipsum')}
-        >
-            <Text>Press me</Text>
-        </TouchableHighlight>
-    </View>
+    return (
+        <View>
+            <Text>Mnemonic: {mm}</Text>
+
+            <TouchableHighlight
+                onPress={() => setMm(entropyToMnemonic(randomBytes(16)))}
+            >
+                <Text>Generate Mnemonic</Text>
+            </TouchableHighlight>
+
+            <TouchableHighlight
+                onPress={() => nav.stateNavigator.navigate('ipsum')}
+            >
+                <Text>Press me</Text>
+            </TouchableHighlight>
+        </View>
+    )
+}
 
 
 module.exports = withNav(Lorem)
