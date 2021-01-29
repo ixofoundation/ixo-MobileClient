bdcf597c1b6138523c096fd919b10900d55e360f
gwn <dev@gwn.wtf>
2021-01-29 21:51:20 +0300

    Configure ESLint in the ignoredWarnings file

--- src/ignoredWarnings.js
+++ src/ignoredWarnings.js
@@ -1,5 +1,5 @@
-// eslint-disable max-len
-//
+/* eslint-disable max-len */
+
 module.exports = [
     /^Setting a timer for a long period of time, i.e. multiple minutes,/,
     // See https://github.com/facebook/react-native/issues/12981#issuecomment-652745831
