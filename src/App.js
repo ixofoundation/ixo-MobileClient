58b81b72d8a70e0d0539090350047890d5f1ac2e
gwn <dev@gwn.wtf>
2021-03-03 20:16:32 +0300

    Integrate react-query for async stuff

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ const
     React = require('react'),
     {NavigationHandler} = require('navigation-react'),
     {NavigationStack} = require('navigation-react-native'),
+    {QueryClient, QueryClientProvider} = require('react-query'),
     nav = require('./nav')
 
 
+const queryClient = new QueryClient()
+
 module.exports = () =>
-    <NavigationHandler stateNavigator={nav}>
-        <NavigationStack />
-    </NavigationHandler>
+    <QueryClientProvider client={queryClient}>
+        <NavigationHandler stateNavigator={nav}>
+            <NavigationStack />
+        </NavigationHandler>
+    </QueryClientProvider>
