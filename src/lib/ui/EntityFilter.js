554c4a47a05545052695dbb80f485a0ac48a3a27
hamzali <hamzalitas95@gmail.com>
2021-03-12 00:05:34 +0300

    add filter layout styles

--- src/lib/ui/EntityFilter.js
+++ src/lib/ui/EntityFilter.js
@@ -1,15 +1,15 @@
 const
     React = require('react'),
     {useState, Children, cloneElement} = React,
-    {View} = require('react-native'),
+    {View, Text, Pressable, StyleSheet} = require('react-native'),
     {noop} = require('lodash-es'),
-    ButtonGroup = require('./ButtonGroup'),
     Button = require('./Button'),
     Select = require('./Select'),
     Switch = require('./Switch'),
-    Heading = require('./Heading'),
     DatePicker = require('./DatePicker'),
-    DateRangePicker = require('./DateRangePicker')
+    DateRangePicker = require('./DateRangePicker'),
+    Icon = require('./Icon'),
+    theme = require('$/theme')
 
 const filterComponents = [
     Select,
@@ -25,34 +25,96 @@ const EntityFilter = ({
     onCancel = noop,
 }) => {
     const [value, setValue] = useState(initialValue)
-
-    return <View>
-        <Heading children='Filter' />
-
-        <ButtonGroup>
-            <Button text='Close' onPress={onCancel}/>
-            <Button text='Reset' onPress={() => setValue({})}/>
-        </ButtonGroup>
+    const itemCount = Children.count(children)
+    return <View style={s.root}>
+        <View style={s.titleContainer}>
+            <Text 
+                children='Filter' 
+                style={s.title}
+            />
+            <Pressable onPress={onCancel}>
+                <Icon name='close' fill='white'/>
+            </Pressable>
+            <Button 
+                prefix={<Icon name='web' fill='#00D2FF'/>}
+                text='Reset'
+                textStyle={s.refreshBtnText}
+                style={s.refreshBtn}
+                onPress={() => setValue({})}
+            />
+        </View>
 
         {
-            Children.map(children, (child) => {
+            Children.map(children, (child, index) => {
                 if (filterComponents.some(comp => comp === child.type)) {
                     const {id} = child.props
-                    return cloneElement(child, {
+                    const filterItem = cloneElement(child, {
                         ...child.props, 
                         key: id,
                         onChange: val =>
                             setValue(prevValue => ({...prevValue, [id]: val})),
                         value: value[id],
                     })
+                    return <>
+                        {filterItem}
+                        {index !== itemCount - 1 && <Divider/>}
+                    </>
                 }
                 return child
             })
         }
 
-        <Button text='Apply' onPress={() => onChange(value)} />
+        <Button 
+            text='Apply' 
+            color='primary'
+            type='contained'
+            style={s.applyBtn}
+            textStyle={s.applyBtnText}
+            onPress={() => onChange(value)} 
+        />
     </View>
 }
 
+const Divider = () => <View style={s.divider}/>
+
+const s = StyleSheet.create({
+    root: {
+        flex: 1,
+        backgroundColor: '#012D42',
+        paddingHorizontal: theme.spacing(2),
+    },
+    title: {
+        position: 'absolute',
+        width: '100%',
+        textAlign: 'center',
+        color: 'white', 
+        fontWeight: 'bold', 
+        fontSize: theme.fontSizes.h4,
+    },
+    titleContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginVertical: theme.spacing(2),
+    },
+    divider: {
+        borderBottomWidth: 1,
+        borderColor: '#023851',
+        width: '100%',
+        marginVertical: theme.spacing(1),
+    },
+    refreshBtn: {
+        flexDirection: 'row', 
+        alignItems: 'center',
+        padding: 0,
+    },
+    refreshBtnText: {
+        color: '#00D2FF',
+        marginHorizontal: 0,
+        marginLeft: theme.spacing(1),
+    },
+    applyBtn: {alignItems: 'center', marginTop: theme.spacing(1)},
+    applyBtnText: {color: 'white'},
+})
 
 module.exports = EntityFilter
