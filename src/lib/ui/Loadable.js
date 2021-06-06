f3bf45de67dbd1657cb52761a23b49465d0b6ce8
hamzali <hamzalitas95@gmail.com>
2021-06-06 20:56:20 +0300

    update loadable styles and indicator

--- src/lib/ui/Loadable.js
+++ src/lib/ui/Loadable.js
@@ -1,5 +1,6 @@
+const {spacing, fontSizes} = require('$/theme')
 const React = require('react'),
-    {View, Text, StyleSheet} = require('react-native')
+    {View, Text, StyleSheet, ActivityIndicator} = require('react-native')
 
 const style = StyleSheet.create({
     container: {
@@ -13,11 +14,36 @@ const Container = ({children}) => (
     <View style={style.container}>{children}</View>
 )
 
-const Loadable = ({data, loading, error, render}) => {
+const bgStyles = StyleSheet.create({
+    error: {
+        borderRadius: 8,
+        backgroundColor: '#E2223B',
+        padding: spacing(1),
+    },
+    errorCode: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: fontSizes.h6,
+    },
+    errorMsg: {
+        color: 'white',
+        fontSize: fontSizes.p1,
+    },
+    empty: {
+        backgroundColor: '#ED9526',
+        padding: spacing(1),
+    },
+    emptyMsg: {
+        color: 'white',
+        fontSize: fontSizes.p1,
+    },
+})
+
+const Loadable = ({data, loading, error, render, empty}) => {
     if (loading) {
         return (
             <Container>
-                <Text>Loading...</Text>
+                <ActivityIndicator size="large" color="#03D0FB" />
             </Container>
         )
     }
@@ -25,15 +51,24 @@ const Loadable = ({data, loading, error, render}) => {
     if (error) {
         return (
             <Container>
-                <Text>Error...</Text>
+                <View style={bgStyles.error}>
+                    {error.code && (
+                        <Text style={bgStyles.errorCode}>{error.code}</Text>
+                    )}
+                    <Text style={bgStyles.errorMsg}>{error.message}</Text>
+                </View>
             </Container>
         )
     }
 
     if (!data) {
-        return (
+        return empty ? (
+            empty
+        ) : (
             <Container>
-                <Text>No Data...</Text>
+                <View style={bgStyles.empty}>
+                    <Text style={bgStyles.emptyMsg}>Empty Result...</Text>
+                </View>
             </Container>
         )
     }
